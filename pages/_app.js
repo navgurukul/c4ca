4fb6a31abdced48428965f7819e3109db9f480b1
Komal Ahire <komala18@navgurukul.org>
2023-09-18 18:33:40 +0530

    added logo

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
         <title>C4CA Board</title>
         <meta name="description" content="Created by Navgurukul" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/c4ca.svg"/>
       </Head>
 
       <ThemeProvider theme={theme}>
