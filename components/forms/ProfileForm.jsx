ea0974e22637d166158aac7ec77f364563590673
Apekshahande <apeksha18@navgurukul.org>
2023-09-25 05:42:59 -0400

    profile form and team form changes

--- components/forms/ProfileForm.jsx
+++ components/forms/ProfileForm.jsx
@@ -1,3 +1,11 @@
+import React, { useState } from 'react';
+import { Camera } from "@mui/icons-material";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { breakpoints } from "@/theme/constant";
+import InputControl from "./InputControl";
+import Team from "./Team";
+import InputLabel from '@mui/material/InputLabel';
 import {
   Avatar,
   Box,
@@ -6,99 +14,361 @@ import {
   Grid,
   Button,
   useMediaQuery,
+  TextField,
+  Select,
+  MenuItem,
 } from "@mui/material";
-import { Camera } from "@mui/icons-material";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { breakpoints } from "@/theme/constant";
-import InputControl from "./InputControl";
-import Team from "./Team";
 
 const ProfileForm = () => {
+  const [formData, setFormData] = useState({
+    name:'',
+    email: '',
+    phoneNumber: '',
+    schoolName: '',
+    district:'',
+    state:''
+  });
+  const states = [
+    {
+      name: 'Jharkhand',
+    },{
+      name: ' Karnataka',
+    },
+    {
+      name: 'Kerala',
+    },
+    {
+      name: ' Maharashtra',
+    }
+  ];
+  // const districts = {
+  //   "Jharkhand": [
+  //     { name: "District 1A" },
+  //     { name: "District 1B" },
+  //     { name: "District 1C" },
+  //   ],
+  //   "Karnataka": [
+  //     { name: "District 1A" },
+  //     { name: "District 1B" },
+  //     { name: "District 1C" },
+  //   ],
+  //   "Kerala": [
+  //     { name: "District 1A" },
+  //     { name: "District 1B" },
+  //     { name: "District 1C" },
+  //   ],
+  //   "Maharashtra": [
+  //     { name: "District 1A" },
+  //     { name: "District 1B" },
+  //     { name: "District 1C" },
+  //   ],
+
+  // }
+
+
+  const [errors, setErrors] = useState({
+    name:'',
+    email: '',
+    phoneNumber: '',
+    schoolName: '',
+    district:'',
+    state:''
+  });
+
+  const validateEmail = (email) => {
+    const regex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    return regex.test(email);
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    // You can customize this regex based on your phone number format
+    const regex = /^\d{10}$/;
+    return regex.test(phoneNumber);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validation for email
+    if (!validateEmail(formData.email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: 'Invalid email address',
+      }));
+      return;
+    }
+
+    // Validation for phone number
+    if (!validatePhoneNumber(formData.phoneNumber)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        phoneNumber: 'Invalid phone number',
+      }));
+      return;
+    }
+
+    if (formData.name.length<3) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        name: 'Name should be atleast 3 characters',
+      }));
+      return;
+    }
+
+    // If all validations pass, you can submit the form
+    console.log('Form submitted with data:', formData);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // Clear error message when the user starts typing
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: '',
+    }));
+    if(name === 'phoneNumber' && value.length > 10) return;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  
+
   const router = useRouter();
   const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
-  return (
-    <>
-      <Container
-        maxWidth="lg"
-        disableGutters
-        sx={{ display: "grid", placeItems: "center", gap: 4 }}
-      >
-        <Typography variant="h5" color="text.primary">
-          {router.asPath === "/profile/profile-update"
-            ? "Personal Details"
-            : "Set Up Your Profile"}
-        </Typography>
 
-        <Container maxWidth="sm" sx={{ display: "grid", gap: 4 }}>
-          <Box className="AvatarBox">
+
+  return (
+    
+    <Container
+            maxWidth="lg"
+            disableGutters
+            sx={{ display: "grid", placeItems: "center", gap  : "20px"}}
+       >
+      <Typography variant="h5" color="text.primary">
+        {router.asPath === "/profile/profile-update"
+          ? "Personal Details"
+          : "Setup Profile"}
+      </Typography>
+      
+      <Container maxWidth="sm">
+      <form onSubmit={handleSubmit}>
+        <Box className="AvatarBox">
             <Avatar src="/avatar.svg" sx={{ width: "100%", height: "100%" }} />
             <Camera className="Camera" />
           </Box>
-
-          <InputControl
-            label="Full Name"
-            type="text"
-            placeholder="Enter Your Name"
+          <Box  sx={{
+            marginBottom: "20px",
+          }}>
+          <InputLabel htmlFor="name" variant='Body2'>Full Name</InputLabel>
+          <TextField
+            fullWidth
+            id="name"
+            name="name"
+           placeholder='Name'
+            variant="outlined"
+            margin="normal"
+            value={formData.name}
+            onChange={handleInputChange}
+            error={!!errors.name}
+            helperText={errors.name}
+            InputProps={{
+                style: {
+                  height: "50px",
+                  borderRadius: "50px",
+                }
+              }}
           />
-
-          <InputControl
-            label="Email Address"
-            type="Email"
-            placeholder="Enter Email Address"
+          </Box>
+          <Box  sx={{
+            marginBottom: "20px",
+          }}>
+          <InputLabel htmlFor="name" variant='Body2'>Email Address</InputLabel>
+          <TextField
+            fullWidth
+            id="email"
+            name="email"
+            placeholder='Email'
+            variant="outlined"
+            margin="normal"
+            value={formData.email}
+            onChange={handleInputChange}
+            error={!!errors.email}
+            helperText={errors.email}
+            InputProps={{
+                style: {
+                   height: "50px",
+                  borderRadius: "50px",
+                }
+              }}
           />
-
-          <Box>
+          </Box>
+          <Box  sx={{
+            marginBottom: "20px",
+          }}>
+          <InputLabel htmlFor="name" variant='Body2'>Phone Number</InputLabel>
+          <TextField
+            fullWidth
+            id="phoneNumber"
+            name="phoneNumber"
+            // label="Phone Number"
+            placeholder='Phone Number'
+            variant="outlined"
+            margin="normal"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+            error={!!errors.phoneNumber}
+            helperText={errors.phoneNumber}
+            InputProps={{
+                style: {
+                  height: "50px",
+                  borderRadius: "50px",
+                }
+              }}
+          />
+          </Box>
+          <Box  sx={{
+            marginBottom: "20px",
+          }}>
+          <InputLabel htmlFor="name" variant='Body2'>School</InputLabel>
+          <TextField
+            fullWidth
+            id="schoolName"
+            name="schoolName"
+            placeholder='School Name'
+            variant="outlined"
+            margin="normal"
+            value={formData.schoolName}
+            onChange={handleInputChange}
+            error={!!errors.schoolName}
+            helperText={errors.schoolName}
+            InputProps={{
+                style: {
+                  height: "50px",
+                  borderRadius: "50px",
+                }
+              }}
+          />
+          </Box>
+          <Box  sx={{
+            marginBottom: "20px",
+          }}>
             <Grid container spacing={isMobile ? 2 : 4}>
               <Grid item md={6} sm={6} xs={12}>
-                <InputControl label="Phone Number" type="tel" maxLength={10} />
-                <Typography variant="body2" color="Grey.main">
-                  As a student, you can enter your parent's phone number
-                </Typography>
+              <InputLabel htmlFor="district" variant='Body2' >District</InputLabel>
+              {/* <Select
+                fullWidth
+                id="district"
+                name="district"
+                value={formData.district}
+                onChange={(e) => {
+                  handleInputChange(e);
+                }}
+                error={!!errors.district}
+                sx={{
+                  height: "50px",
+                  borderRadius: "50px",
+                }}
+              >
+                <MenuItem value="">Select District</MenuItem>
+                {formData.state &&
+                  districts[formData.state].map((district) => (
+                    <MenuItem key={district.name} value={district.name}>
+                      {district.name}
+                    </MenuItem>
+                  ))}
+              </Select> */}
+                 <TextField
+                  fullWidth
+                  id="district"
+                  name="district"
+                  placeholder='Select District'
+                  variant="outlined"
+                  margin="normal"
+                  value={formData.district}
+                  onChange={handleInputChange}
+                  error={!!errors.district}
+                  helperText={errors.district}
+                  InputProps={{
+                      style: {
+                        height: "50px",
+                        borderRadius: "50px",
+                      }
+                    }}
+                /> 
+                
+
+                
               </Grid>
               <Grid item md={6} sm={6} xs={12}>
-                <InputControl label="Date of Birth" type="date" />
-                <Typography variant="body2" color="Grey.main">
-                  On or after 1 April 1995
-                </Typography>
+
+                <InputLabel htmlFor="state" variant='Body2' sx={{
+                  marginBottom: "15px",
+                }}  >State</InputLabel>      
+                  <Select
+                    fullWidth
+                    id="state"
+                    name="state"
+                    value={formData.state}
+                    onChange={handleInputChange}
+                    error={!!errors.state}
+                  sx={{
+                    height: "50px",
+                    borderRadius: "50px",
+                  }}
+                  >
+                    <MenuItem value="" defaultValue ="Select State"></MenuItem>
+                    {states.map((state) => (
+                      <MenuItem key={state.name} value={state.name}>
+                        {state.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
               </Grid>
             </Grid>
           </Box>
+          <Grid   container justifyContent="center">
+            {formData.name !== '' &&
+              formData.email !== '' &&
+              formData.phoneNumber !== '' &&
+              formData.schoolName !== '' ? <Button
+              type="submit"
+              className="profileBtn"
+            
+            >
+              Save and Proceed
+            </Button>:
+            <Button
+              // className="profileBtn"
+              variant="contained"
+              disabled
+            >
+              Save and Proceed
+            </Button>
+                }
 
-          <Box
-            className={
-              router.asPath === "/profile/profile-update" ? "show" : "hide"
-            }
-          >
-            <Grid container spacing={isMobile ? 2 : 4}>
-              <Grid item md={6} sm={6} xs={12}>
-                <InputControl label="School" type="text" />
-              </Grid>
-              <Grid item md={6} sm={6} xs={12}>
-                <InputControl label="Class" type="text" />
-              </Grid>
-              <Grid item md={12} sm={12} xs={12}>
-                <InputControl label="State" type="text" />
-              </Grid>
-            </Grid>
-          </Box>
-        </Container>
+          </Grid>
 
-        {router.asPath === "/profile/profile-update" ? <Team /> : null}
-
-        <Button className="profileBtn">
-          {router.asPath === "/profile/profile-update" ? (
-            <Link href="/profile/profile-update">
-              <Typography variant="ButtonLarge">Save Profile</Typography>
-            </Link>
-          ) : (
-            <Link href="/dashboard">
-              <Typography variant="ButtonLarge">Save & Proceed</Typography>
-            </Link>
-          )}
-        </Button>
+         
+          
+         
+            {router.asPath === "/profile/profile-update" ? <Team /> : null}
+  
+            {/* {router.asPath === "/profile/profile-update" ? (
+             <Link href="/profile/profile-update">
+               <Typography variant="ButtonLarge">Save Profile</Typography>
+             </Link>
+           ) : (
+             <Link href="/dashboard">
+               <Typography variant="ButtonLarge">Save & Proceed</Typography>
+             </Link>
+           )}  */}
+        </form>
       </Container>
-    </>
+    </Container>
   );
 };
 
--- components/forms/Team.jsx
+++ components/forms/Team.jsx
@@ -1,73 +1,410 @@
-import {
-  Container,
-  Button,
-  Typography,
-  Box,
-  useMediaQuery,
-} from "@mui/material";
-import InputControl from "./InputControl";
+import React, { useState, useEffect } from 'react';
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { breakpoints } from "@/theme/constant";
+import InputLabel from '@mui/material/InputLabel';
+import Divider from '@mui/material/Divider';
+import {
+  Box,
+  Container,
+  Typography,
+  Grid,
+  Button,
+  useMediaQuery,
+  TextField,
+  Select,
+  MenuItem,
+  
+} from "@mui/material";
 
 const Team = () => {
-  const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
+  const [formData, setFormData] = useState({
+    name: '',
+    schoolName: '',
+    district: '',
+    state: '',
+    class: '',
+  });
 
-  const handleClick = (e) => {
-    console.log(e.target.textContent);
+  const states = [
+    {
+      name: 'Jharkhand',
+    }, {
+      name: ' Karnataka',
+    }, {
+      name: 'Kerala',
+    }, {
+      name: ' Maharashtra',
+    }
+  ];
+
+  const classes = [
+    {
+      name: '1st',
+    }, {
+      name: '2nd',
+    }, {
+      name: '3rd',
+    }, {
+      name: ' 4th',
+    }, {
+      name: '5th',
+    }, {
+      name: '6th',
+    }
+  ];
+
+  const [errors, setErrors] = useState({
+    name: '',
+    schoolName: '',
+    district: '',
+    state: '',
+    class: ''
+  });
+
+  const [teamSize, setTeamSize] = useState(3);
+const [selectedTeamSize, setSelectedTeamSize] = useState(3);
+
+
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: '',
+    }));
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
+  const handleTeamSizeChange = (size) => {
+    setTeamSize(size);
+    setSelectedTeamSize(size);
+
+  };
+
+  const validateStudentFields = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    for (let i = 1; i <= teamSize; i++) {
+      const studentNameKey = `studentName${i}`;
+      const classKey = `class${i}`;
+
+      if (!formData[studentNameKey]) {
+        newErrors[studentNameKey] = 'Student name is required';
+        isValid = false;
+      }
+
+      if (!formData[classKey] || formData[classKey] === 'Select class') {
+        newErrors[classKey] = 'Class is required';
+        isValid = false;
+      }
+    }
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      ...newErrors,
+    }));
+
+    return isValid;
+  };
+
+  const isFormValid = () => {
+    // Check if team name and school name are not empty
+    if (formData.name === '' || formData.schoolName === '') {
+      return false;
+    }
+
+    // Check if there are no errors for student names and classes
+    for (let i = 1; i <= teamSize; i++) {
+      const studentNameKey = `studentName${i}`;
+      const classKey = `class${i}`;
+      if (errors[studentNameKey] || errors[classKey]) {
+        return false;
+      }
+    }
+
+    return true; // Form is valid if all checks pass
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    if (formData.name.length < 3) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        name: 'Name should be at least 3 characters',
+      }));
+      return;
+    }
+    if (formData.schoolName.length < 3) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        schoolName: 'Name should be at least 3 characters',
+      }));
+      return;
+    }
+
+    const isStudentFieldsValid = validateStudentFields();
+
+    if (!isStudentFieldsValid) {
+      return;
+    }
+
+    console.log('Form submitted with data:', formData);
+  };
+
+  const renderStudentFields = () => {
+    const studentFields = [];
+    for (let i = 1; i <= teamSize; i++) {
+      const studentNameKey = `studentName${i}`;
+      const classKey = `class${i}`;
+
+      studentFields.push(
+        <Grid container spacing={isMobile ? 2 : 4} key={i}>
+          <Grid item md={6} sm={6} xs={12}>
+            <InputLabel htmlFor={studentNameKey} variant='body2'>
+              Student Name {i}
+            </InputLabel>
+            <TextField
+              fullWidth
+              id={studentNameKey}
+              name={studentNameKey}
+              placeholder={`Student Name ${i}`}
+              variant="outlined"
+              margin="normal"
+              value={formData[studentNameKey] || ''}
+              onChange={handleInputChange}
+              error={!!errors[studentNameKey]}
+              helperText={errors[studentNameKey] || ''}
+              InputProps={{
+                style: {
+                  height: "50px",
+                  borderRadius: "50px",
+                },
+              }}
+            />
+          </Grid>
+          <Grid item md={6} sm={6} xs={12}>
+            <InputLabel htmlFor={classKey} variant='body2' sx={{ marginBottom: "15px" }}>
+              Class {i}
+            </InputLabel>
+            <Select
+              fullWidth
+              id={classKey}
+              name={classKey}
+              placeholder={`Class ${i}`}
+              value={formData[classKey] || ''}
+              onChange={handleInputChange}
+              error={!!errors[classKey]}
+              sx={{
+                height: "50px",
+                borderRadius: "50px",
+              }}
+            >
+              <MenuItem value="" defaultValue="Select class"></MenuItem>
+              {classes.map((std) => (
+                <MenuItem key={std.name} value={std.name}>
+                  {std.name}
+                </MenuItem>
+              ))}
+            </Select>
+            {errors[classKey] && (
+              <Typography variant="caption" color="error">
+                {errors[classKey]}
+              </Typography>
+            )}
+          </Grid>
+        </Grid>
+      );
+    }
+    return studentFields;
+  };
+
+  const router = useRouter();
+  const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
+
   return (
-    <Container maxWidth="sm" sx={{ display: "grid", gap: isMobile ? 2 : 4 }}>
+    <Container
+      maxWidth="lg"
+      disableGutters
+      sx={{ display: "grid", placeItems: "center", gap: "20px" }}
+    >
       <Typography variant="h5" color="text.primary">
-        Team Details
+        Add a Team
       </Typography>
 
-      <Box sx={{ display: "grid", gap: 1 }}>
-        <Typography variant="body2" color="text.primary">
-          Select Team Size
-        </Typography>
-
-        <Box className="btnGrp">
-          <Button className="teamBtn" variant="subtitle1" onClick={handleClick}>
-            3
-          </Button>
-          <Button className="teamBtn" variant="subtitle1" onClick={handleClick}>
-            4
-          </Button>
-          <Button className="teamBtn" variant="subtitle1" onClick={handleClick}>
-            5
-          </Button>
+      <Container maxWidth="sm" >
+        <Box sx={{
+          marginBottom: "20px",
+        }}>
+          <InputLabel htmlFor="name" variant='body2'>Team Name</InputLabel>
+          <TextField
+            fullWidth
+            id="name"
+            name="name"
+            placeholder='Team Name'
+            variant="outlined"
+            margin="normal"
+            value={formData.name}
+            onChange={handleInputChange}
+            error={!!errors.name}
+            helperText={errors.name || ''}
+            InputProps={{
+              style: {
+                height: "50px",
+                borderRadius: "50px",
+              }
+            }}
+          />
         </Box>
-      </Box>
 
-      <Typography variant="body2" color="Grey.main">
-        Your team members will join your team when they login or see your invite
-        if they already registered on the platform
-      </Typography>
+        <Box sx={{
+          marginBottom: "20px",
+        }}>
+          <InputLabel htmlFor="schoolName" variant='body2'>School</InputLabel>
+          <TextField
+            fullWidth
+            id="schoolName"
+            name="schoolName"
+            placeholder='School Name'
+            variant="outlined"
+            margin="normal"
+            value={formData.schoolName}
+            onChange={handleInputChange}
+            error={!!errors.schoolName}
+            helperText={errors.schoolName || ''}
+            InputProps={{
+              style: {
+                height: "50px",
+                borderRadius: "50px",
+              }
+            }}
+          />
+        </Box>
+        <Box sx={{
+          marginBottom: "20px",
+        }}>
+          <Grid container spacing={isMobile ? 2 : 4}>
+            <Grid item md={6} sm={6} xs={12}>
+              <InputLabel htmlFor="state" variant='body2' sx={{
+                marginBottom: "15px",
+              }}  >State</InputLabel>
+              <Select
+                fullWidth
+                id="state"
+                name="state"
+                value={formData.state}
+                onChange={handleInputChange}
+                error={!!errors.state}
+                sx={{
+                  height: "50px",
+                  borderRadius: "50px",
+                }}
+              >
+                <MenuItem value="" defaultValue="Select State"></MenuItem>
+                {states.map((state) => (
+                  <MenuItem key={state.name} value={state.name}>
+                    {state.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item md={6} sm={6} xs={12}>
+              <InputLabel htmlFor="district" variant='body2' >District</InputLabel>
+              <TextField
+                fullWidth
+                id="district"
+                name="district"
+                placeholder='Select District'
+                variant="outlined"
+                margin="normal"
+                value={formData.district}
+                onChange={handleInputChange}
+                error={!!errors.district}
+                helperText={errors.district || ''}
+                InputProps={{
+                  style: {
+                    height: "50px",
+                    borderRadius: "50px",
+                  }
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+        <Box sx={{
+          marginBottom: "20px",
+        }}>
+          <Divider />
+        </Box>
 
-      <InputControl label="Team Name" type="text" />
+        <Box sx={{
+          marginBottom: "20px",
+        }}>
+          <Typography variant="body2" color="Grey.main" sx={{
+            marginBottom: "10px",
+          }}>
+            Select Team Size
+          </Typography>
+          <Box className="btnGrp">
+            <Button className={`teamBtn ${selectedTeamSize === 3 ? 'selected' : ' ' }`}
+variant="subtitle1" onClick={() => handleTeamSizeChange(3)}>
+              3
+            </Button>
+            <Button className={`teamBtn ${selectedTeamSize === 4 ? 'selected' : ' ' }`}
+variant="subtitle1" onClick={() => handleTeamSizeChange(4)}>
+              4
+            </Button>
+            <Button className={`teamBtn ${selectedTeamSize === 5? 'selected' : ' ' }`}
+variant="subtitle1" onClick={() => handleTeamSizeChange(5)}>
+              5
+            </Button>
+          </Box>
+        </Box>
+        <Typography variant="body2" color="Grey.main" sx={{ marginBottom: "10px", }}>
+          User ID and password will be created automatically and shareable from the dashboard
+        </Typography>
 
-      <Box sx={{ display: "grid", gap: 1 }}>
-        <Typography variant="body2" color="text.primary">
-          Team Member 1
+        <Box sx={
+          {
+            marginBottom:"10px"
+          }
+        }>
+          {renderStudentFields()}
+        </Box>
+        <Typography variant="body2" color="Grey.main" sx={{ marginBottom: "10px", }}>
+          If you do not have the student details, you can skip this step and add a team later
         </Typography>
-        <Typography variant="body1" color="text.primary">
-          User Name (You)
-        </Typography>
-      </Box>
-      <Box sx={{ display: "grid", gap: 1 }}>
-        <Typography variant="body2" color="text.primary">
-          Team Member 2
-        </Typography>
-        <InputControl label="Full Name" type="text" />
-        <InputControl label="Email Address" type="email" />
-      </Box>
-      <Box sx={{ display: "grid", gap: 1 }}>
-        <Typography variant="body2" color="text.primary">
-          Team Member 3
-        </Typography>
-        <InputControl label="Full Name" type="text" />
-        <InputControl label="Email Address" type="email" />
-      </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Link href="/profile">
+              <Button variant="outlined" >Back</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={2}>
+            <Link href="/dashboard">
+              <Button variant="outlined" >Skip</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              variant="contained"
+              type="submit"
+              color="primary"
+              onClick={submit}
+              disabled={!isFormValid()} // Disable the button if the form is not valid
+            >
+              Add a Team
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     </Container>
   );
 };
