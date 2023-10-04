afe69b1ca8de28fd5cbdc6868e4e22e1ddb850cf
Apekshahande <apeksha18@navgurukul.org>
2023-10-04 12:46:50 +1100

    created login form

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+// import LoginForm from "@/components/forms/LoginForm";
 
 export default function Home() {
   // const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)")
   return (
     <main>
       <h1>Home Page...</h1>  
+      {/* <LoginForm /> */}
     </main>
   )
 }
