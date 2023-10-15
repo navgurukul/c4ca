a4e6b0f90677ca79ecf8251347c08bb230bf1aec
Apekshahande <apeksha18@navgurukul.org>
2023-10-16 10:45:57 +1100

    created dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Dashboard from "@/components/dashboard/Dashboard";
 
 export default function Home() {
   // const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)")
   return (
     <main>
-      <h1>Home Page...</h1>  
+      {/* <h1>Home Page...</h1>   */}
+      <Dashboard />
     </main>
   )
 }
