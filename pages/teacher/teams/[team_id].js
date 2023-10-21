c0cd67fd386f01e13eae5c7c66286d986f40275b
Apekshahande <apeksha18@navgurukul.org>
2023-10-21 12:10:51 +1100

    fixed team_id page

--- pages/teacher/teams/[team_id].js
+++ pages/teacher/teams/[team_id].js
@@ -18,36 +18,6 @@ import Divider from "@mui/material/Divider";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
 
-const teamMemberData = [
-  {
-    id: 1,
-    name: "Arti Singh",
-    class: 5,
-  },
-  {
-    id: 2,
-    name: "Ravi Chandra Sharma",
-    class: 5,
-  },
-  {
-    id: 3,
-    name: "Mani Kumar Sahu",
-    class: 5,
-  },
-];
-// const teamDetailData = [
-//   {
-//     id: 1,
-//     name: "Flying Color",
-//     user: "flyingcolors123",
-//     password: "gistofthelock",
-//     progress: 100,
-//     current_lesson: "Intro to Scratch of Module 1",
-//     project_status: "Submitted",
-//     link: "https://scratch.merakilearn.org/team2/",
-//   },
-// ];
-
 const TeamDetail = () => {
   const [open, setOpen] = useState(false);
   const [data, setData] = useState({});
@@ -73,8 +43,9 @@ const TeamDetail = () => {
   };
 
   useEffect(() => {
-    if (!params?.team_id) return;
-    const apiUrl = `https://merd-api.merakilearn.org/c4ca/team/${params?.team_id}`;
+    // if (!params?.team_id) return;
+    // const apiUrl = `https://merd-api.merakilearn.org/c4ca/team/${params?.team_id}`;
+    const apiUrl = `https://merd-api.merakilearn.org/c4ca/team/50`;
 
     axios
       .get(apiUrl)
@@ -88,281 +59,103 @@ const TeamDetail = () => {
       });
   }, [params]);
 
-
-
-  console.log(data, "data-------teammmmmm");    
+  console.log(data, "data-------teammmmmm"); 
+  const teamMemberData = data.team_members || [];
 
   return (
     <Container maxWidth="lg" sx={{ mt: 10 }}>
-      <Grid container spacing={2}>
-        <h1>{data.team_name}</h1>
-        
-        <Grid item xs={12} md={7} lg={7}>
-          {/* {team.map((team) => ( */}
-            <Box key={data.id}>
-              <Box sx={{ display: "flex", alignItems: "center", mb: 2, ml: 2 }}>
-                <Typography variant="body1">
-                  Dashboard{" "}
-                  <Typography
-                    variant="body1"
-                    component="span"
-                    sx={{ color: "#29458C" }}
-                  >
-                    / {data.team_name}
-                  </Typography>
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  marginTop: "4px",
-                  ml: 2,
-                  mt: "32px",
-                }}
-              >
-                <Typography variant="h6">{data.team_name}</Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  marginTop: "4px",
-                  gap: 2,
-                  mt: "32px",
-                  mb: "16px",
-                  ml: 2,
-                }}
-              >
-                <Typography variant="subtitle1">Team Login Details</Typography>
-                <ClickAwayListener onClickAway={handleTooltipClose}>
-                  <>
-                    <Tooltip
-                      PopperProps={{
-                        disablePortal: true,
-                      }}
-                      onClose={handleTooltipClose}
-                      open={open}
-                      disableFocusListener
-                      disableHoverListener
-                      disableTouchListener
-                      placement="top"
-                      title="Copy"
-                    >
-                      <Link
-                        underline="none"
-                        sx={{
-                          display: "flex",
-                          alignItems: "flex-start",
-                          marginTop: "4px",
-                        }}
-                        onClick={() => {
-                          copyToClipboard(
-                            `User ID: ${team.user}\nPassword: ${data.password}`
-                          );
-                        }}
-                      >
-                        <CopyAllIcon />
-                        <Typography variant="subtitle2">Copy</Typography>
-                      </Link>
-                    </Tooltip>
-                  </>
-                </ClickAwayListener>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  marginTop: "4px",
-                  mb: "32px",
-                  gap: "32px",
-                  ml: 2,
-                }}
-              >
-                <Grid container>
-                  <Grid item xs={4}>
-                    <Box
-                      sx={{ display: "flex", alignItems: "flex-start", gap: 2 }}
-                    >
-                      <img src="/assets/icon-id.svg" alt="id" />
-                      <Typography>{data.user}</Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Box
-                      sx={{ display: "flex", alignItems: "flex-start", gap: 2 }}
-                    >
-                      <img src="/assets/security-password.svg" alt="passwrod" />
-                      <Typography>{data.password}</Typography>
-                    </Box>
-                  </Grid>
-                </Grid>
-              </Box>
-              <Box>
-                <Divider variant="middle" color="#DEDEDE" />
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  marginTop: "4px",
-                  gap: 1,
-                  mt: "32px",
-                  mb: "16px",
-                  ml: 2,
-                }}
-              >
-                <Typography variant="subtitle1">Course Progress:</Typography>
-                <CircularProgress
-                  variant="determinate"
-                //   value={team.progress}
-                  color="success"
-                  size={25}
-                  thickness={6}
-                />
-                <Typography variant="body1" mr={3}>
-                  {data.progress} %
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  marginTop: "4px",
-                  mb: "32px",
-                  ml: 2,
-                }}
-              >
-                <Grid container>
-                  <Typography
-                    variant="body1"
-                    component="div"
-                    sx={{
-                      display: "flex",
-                      alignItems: "flex-start",
-                      margin: "0",
-                    }}
-                  >
-                    Currently at Lesson:
-                    {/* <Link
-                      href={team.current_lesson}
-                      underline="none"
-                      sx={{ display: "flex", alignItems: "flex-start", ml: 1 }}
-                    >
-                      <Typography variant="body1">
-                        {data.current_lesson}
-                      </Typography>
-                    </Link> */}
-                  </Typography>
-                </Grid>
-              </Box>
-              <Box>
-                <Divider variant="middle" color="#DEDEDE" />
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  marginTop: "4px",
-                  gap: 1,
-                  mt: "32px",
-                  mb: "16px",
-                  ml: 2,
-                }}
-              >
-                <Typography variant="subtitle1">Project Status:</Typography>
-                <FiberManualRecordIcon color="success" />
-                <Typography variant="body1">Submitted</Typography>
-              </Box>
-              {/* <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  marginTop: "4px",
-                  mb: "32px",
-                  ml: 2,
-                }}
-              >
-                <Grid container>
-                  <Typography
-                    variant="body1"
-                    component="div"
-                    sx={{
-                      display: "flex",
-                      alignItems: "flex-start",
-                      margin: "0",
-                    }}
-                  >
-                    Link :
-                    <Link href={team.link} underline="always" ml={1}>
-                      <Typography variant="body1">{data.link}</Typography>
+      <Grid container spacing={2}>    
+        <Grid item xs={12} md={7} lg={7}  >              
+          <Typography variant="body1" sx={{ display: "flex", alignItems: "center", gap: 1 ,mb:2, ml:2}}> Dashboard{" "}
+            <Typography variant="body1" component="span" sx={{ color: "#29458C" }}>/ {data.team_name}</Typography>
+          </Typography>
+          <Typography variant="h6" sx={{ display: "flex", alignItems: "center", gap: 1,mt:"32px" ,ml:2}}>{data.team_name}</Typography>
+          <Box >
+            <Box sx={{display: "flex", alignItems: "flex-start",marginTop: "4px",gap: 2,mt: "32px",mb: "16px",ml: 2,}}>
+              <Typography variant="subtitle1">Team Login Details</Typography>
+              <ClickAwayListener onClickAway={handleTooltipClose}>
+                  <Tooltip onClose={handleTooltipClose} open={open} disableFocusListener disableHoverListener disableTouchListener placement="top" title="Copy" PopperProps={{disablePortal: true,}}>
+                    <Link underline="none" sx={{display: "flex",alignItems: "flex-start", marginTop: "4px",}}
+                      onClick={() => {copyToClipboard(`User ID: ${data.login_id}\nPassword: ${data.password}`);}}>
+                      <CopyAllIcon />
+                      <Typography variant="subtitle2">Copy</Typography>
                     </Link>
-                  </Typography>
-                </Grid>
-              </Box> */}
+                  </Tooltip>     
+              </ClickAwayListener>
             </Box>
-          {/* ))} */}
+            <Grid container sx ={{mb:"32px", }}>
+              <Grid item xs={4} ml={2}>
+                <Box sx={{ display: "flex", alignItems: "flex-start", gap: 2 }}>
+                  <img src="/assets/icon-id.svg" alt="id" />
+                  <Typography>{data.login_id}</Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={4}>
+                <Box sx={{ display: "flex", alignItems: "flex-start", gap: 2 }}>
+                  <img src="/assets/security-password.svg" alt="passwrod" />
+                  <Typography>{data.password}</Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+          <Divider variant="middle" color="#DEDEDE"  alignItems= "flex-start" /> 
+          <Box ml={ 2}>
+            <Grid container sx ={{ mb:"16px",mt:"32px"}}>
+                <Typography variant="subtitle1" sx={{ mr:2}}>Course Progress:</Typography>
+                <CircularProgress variant="determinate" value={100} color="success" size={25} thickness={6}  sx={{ mr:1}}/>
+                <Typography variant="body1" mr={3}>{100} %</Typography>
+            </Grid>   
+            <Grid container mb="32px">
+              <Typography variant="body1" component="div" sx={{display: "flex",}}>
+                Currently at Lesson:
+                <Link href='{team.current_lesson}' underline="none" sx={{ display: "flex", alignItems: "flex-start", ml: 1 }}>
+                  <Typography variant="body1">Intro to Scratch of Module 1</Typography>
+                </Link>
+              </Typography>
+            </Grid>  
+          </Box>
+          <Divider variant="middle" color="#DEDEDE" alignItems= "flex-start" />
+          <Box ml={2}>
+            <Grid container sx ={{ mb:"16px",mt:"32px"}}>
+              <Typography variant="subtitle1"  sx={{display: "flex", mr:2}}>Project Status:</Typography>
+              <FiberManualRecordIcon color="success" sx={{ mr:1}} />
+              <Typography variant="body1">Submitted</Typography>
+            </Grid>   
+            <Grid container mb="32px">
+              <Typography variant="body1" component="div" sx={{display: "flex",}}>
+                Link:
+                <Link href='{team.current_lesson}' underline="none" sx={{ display: "flex", alignItems: "flex-start", ml: 1 }}>
+                  <Typography variant="body1">https://scratch.merakilearn.org/team2</Typography>
+                </Link>
+              </Typography>
+            </Grid>
+          </Box>  
+            
         </Grid>
          <Grid item xs={12} md={4} lg={4} mt={9}>
           <Card sx={{ border: 1, borderColor: "gray", borderRadius: "8px" }}>
             <CardContent>
-              <Grid
-                container
-                spacing={3}
-                alignItems="flex-start"
-                sx={{ mt: 1 }}
-              >
-                <Grid item xs={6} sx={{ alignSelf: "flex-start" }}>
+              <Grid container spacing={3} alignItems="flex-start" sx={{ mt: 1 }}>
+                <Grid item  sx={{ alignSelf: "flex-start" }}>
                   <Typography variant="subtitle1">Team Members</Typography>
                 </Grid>
-                <Grid
-                  item
-                  xs={6}
-                  sx={{ display: "flex", justifyContent: "flex-end" }}
-                >
-                  <Box
-                    spacing={3}
-                    sx={{ display: "flex", alignItems: "flex-end" }}
-                  >
-                    <img src="/assets/Student.svg" alt="Team Image" />
-                    <Typography>{teamMemberData.length}</Typography>
+                <Grid item xs={6} sx={{ display: "flex", justifyContent: "flex-end" }}>
+                  <Box spacing={3} sx={{ display: "flex", alignItems: "flex-end" }}>
+                    <img src="/assets/Student.svg" alt="Team Image"  />
+                    <Typography variant="body1" sx ={{ml:2}}>{teamMemberData.length}</Typography>
                   </Box>
                 </Grid>
               </Grid>
               {teamMemberData.map((team, index) => (
                 <Grid container spacing={3} sx={{ mt: 1 }} key={index}>
                   <Grid item>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "flex-start",
-                      }}
-                    >
-                      <Typography variant="body2">
-                        Student Name {index + 1}
-                      </Typography>
-                      <Box
-                        sx={{
-                          display: "flex",
-                          alignItems: "flex-start",
-                          marginTop: "4px",
-                        }}
-                      >
-                        <Typography
-                          variant="body1"
-                          sx={{ marginRight: "9px", alignSelf: "flex-start" }}
-                        >
+                    <Box sx={{ display: "flex", flexDirection: "column",alignItems: "flex-start",}}>
+                      <Typography variant="body2">Student Name {index + 1}</Typography>
+                      <Box sx={{ display: "flex", alignItems: "flex-start", marginTop: "10px",}}>
+                        <Typography variant="body1" sx={{ mr: "10px", alignSelf: "flex-start" }}>
                           {team.name}
                         </Typography>
-                        <Chip
-                          label={`Class ${team.class}`}
-                          size="small"
-                          sx={{
-                            backgroundColor: "#FFAD33",
-                            alignSelf: "flex-start",
-                          }}
-                        />
+                        <Chip label={`Class ${team.class}`} size="small" sx={{backgroundColor: "#FFAD33",alignSelf: "flex-start",}}/>
                       </Box>
                     </Box>
                   </Grid>
