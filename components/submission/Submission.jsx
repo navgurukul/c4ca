6ac1944aabcd10ae5de48b5e858726f0346b1296
Apekshahande <apeksha18@navgurukul.org>
2023-10-21 13:27:10 +1100

    added functionality

--- components/submission/Submission.jsx
+++ components/submission/Submission.jsx
@@ -18,46 +18,39 @@ import Link from "@mui/material/Link";
 
 
 const Submission = (props) => {
-  const [show, setShow] = useState(false);
   const [inputControlValue, setInputControlValue] = useState("");
   const [dragDropZoneValue, setDragDropZoneValue] = useState([]);
   const [linkShow , setLinkShow ] = useState(true);
   const [projectShow , setprojectShow ] = useState(true);
-  var inputValue ;
-  var dropValue
 
-  const handleShow = () => setShow(true);
   const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px");
   const handleSubmit = () => {
     setLinkShow(false);
     setprojectShow(false);
   };
 
+  console.log("value", props.values)
   const isSubmitDisabled = !inputControlValue && dragDropZoneValue.length === 0;
 
   const handleInputControlChange = (value) => {
     setInputControlValue(value);
-    console.log("val",inputControlValue);
-    
+    console.log(inputControlValue)
+    // value is not showing on conosle  
   };
 
   const handleDragDropZoneChange = (files) => {
     setDragDropZoneValue(files);
-    console.log("val",dragDropz);
+    console.log("file" , dragDropZoneValue)
+    // file data is not showing. 
   };
 
   return (
     <>
-      {!show ? (
         <Container
           maxWidth="lg"
           disableGutters
           sx={{ display: "grid", placeItems: "center", gap: 4 }}
         >
-          {/* <Typography variant="h5" color="text.primary">
-            {props.show ? "Re-Submit Project" : "Your Submission"}
-          </Typography> */}
-
           <Container
             maxWidth="sm"
             sx={{ display: "grid", gap: isMobile ? 2 : 4 }}
@@ -116,17 +109,14 @@ const Submission = (props) => {
                 Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. 
                 Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim.
                 Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.
-              </Typography>
-              
+              </Typography>             
             </Box>
             {linkShow&&
-               <>
-           
+               <>   
                 {!isSubmitDisabled&&
                 <>
                   <Divider />
                   <Typography variant="body1">Draft saved on 23 Sep 2023</Typography>
-
                 </>
                 }
               </>
@@ -137,42 +127,39 @@ const Submission = (props) => {
               </>:
               <>
                 <Typography variant="subtitle1">Scratch Project Link</Typography>
-                <Link href={inputValue}>{inputValue}</Link>
-              
+                <Link href="https://Aliqua id fugiat nostrud irure ex duis ea qui" variant="body1"> https://Aliqua id fugiat nostrud irure ex duis ea quis</Link>             
               </>
              }
             {projectShow?
-              <Box sx={{ display: "grid", gap: 1 }}>
+              <Box sx={{ display: "grid", }}>
                 <Typography variant="body2">Or, Upload project file</Typography>
                 <DragDropZone onChange={handleDragDropZoneChange} />
-              </Box>:
+              </Box>:dragDropZoneValue !==""&&(
               <>
                 <Typography variant="subtitle1">Scratch Project File</Typography>
-                <Link to={dropValue}>{dropValue}</Link>
-              
-              </>
+                <Box className="drop-file-preview__item__info">
+                  <img src="/project.svg" alt="" />
+                  <Typography variant="body1" color='text.primary' >{dragDropZoneValue}Hello world.sb3</Typography>
+                </Box>            
+              </>)
             }
             {!linkShow&&!projectShow&& 
               <Grid container spacing={2}>
                 <Grid item xs={12} container justifyContent="center" alignItems="center">
-                    <Button size="medium" variant="subtitle1"  className="profileBtn">
-                    Return to Dashboard
+                    <Button size="medium"   className="profileBtn">
+                    <Link href="#" underline="none" color={'white'} pl='16px' pr="16px">  Return to Dashboard</Link>
                     </Button>
                 </Grid>
               </Grid> 
-            }
-              
+            }             
           </Container>
           {linkShow&&
           <Container maxWidth="sm" align="center">
           <Grid container  spacing={1}>
                 <Grid item xs={12} sm={6} md={6}>
-                  <Button variant="outlined" 
-                   sx={{
-                    backgroundColor: (theme) => (isSubmitDisabled ? theme.palette.grey[100] : "transparent"),
-                  }}
-                   disabled={!inputControlValue && dragDropZoneValue.length === 0}>
-                    <Typography  variant="ButtonLarge" pl ="16px " pr="16px"pt="8px" pb="8px">Save Draft</Typography>
+                  <Button variant="outlined" sx={{backgroundColor: (theme) => (isSubmitDisabled ? theme.palette.grey[100] : "transparent"),}}
+                    disabled={!inputControlValue && dragDropZoneValue.length === 0}>
+                    <Typography  variant="ButtonLarge" pl ="35px " pr="35px" pt="8px" pb="8px">Save Draft</Typography>
                   </Button>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} >
@@ -180,10 +167,13 @@ const Submission = (props) => {
                   onClick={handleSubmit}
                   disabled={isSubmitDisabled}
                   sx={{
+                    pl:isSubmitDisabled &&"35px ",
+                    pr:isSubmitDisabled &&"35px ",
+                    pt:isSubmitDisabled &&"8px",
+                    pb:isSubmitDisabled &&"8px",
                     backgroundColor: (theme) => (isSubmitDisabled ? theme.palette.grey[500] : "transparent"),
                   }} 
-                  >
-                 
+                  >           
                 <Typography  variant="ButtonLarge" onClick={handleSubmit}>Submit Project</Typography>
               </Button>
                   
@@ -193,9 +183,7 @@ const Submission = (props) => {
           </Container>
           }
         </Container>
-      ) : (
-        <ProjectSubmitted />
-      )}
+      
     </>
   );
 };
