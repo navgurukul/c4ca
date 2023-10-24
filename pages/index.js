59b78e7a81079f289ea348d5dd56e1adde8b882e
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-10-24 08:54:37 +0530

    added loader

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,10 @@
 import { Button, Typography } from "@mui/material";
 
-import { useRouter } from "next/router";
 import Link from "next/link";
 
 
 export default function Home() {
   // const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)")
-
-  
-  const router = useRouter();
-
-  const { token } = router.query;
-  console.log(token, "token from home page")
   return (
     <>
     
