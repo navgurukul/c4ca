aea99ea77125bf4ad950b6bfd707917b1e07e636
komalahire <komala@navgurukul.org>
2023-10-27 10:34:56 +0530

    Added card lock and unlock functionality

--- components/studentDashboard/Module.jsx
+++ components/studentDashboard/Module.jsx
@@ -1,125 +1,174 @@
-import React, { useState,useEffect } from 'react';
-import { Box, Typography, CircularProgress, Button, Grid, Card, CardContent, CardMedia } from '@mui/material';
-import jsonData from '../../data/data.json';
-import LockIcon from '@mui/icons-material/Lock'; // Import the Lock icon
-import LaunchIcon from '@mui/icons-material/Launch'; // Import the Launch icon
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Typography,
+  CircularProgress,
+  Button,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
+import jsonData from "../../data/data.json";
+import LockIcon from "@mui/icons-material/Lock"; // Import the Lock icon
+import LaunchIcon from "@mui/icons-material/Launch"; // Import the Launch icon
 // import customAxios from "../../../api"; // Import your custom Axios instance
-import customAxios from '@/api';
+import customAxios from "@/api";
 
 const Module = () => {
-const [openedCards, setOpenedCards] = useState(0);
-const [data, setData] = useState({});
-
-const handleCardOpen = () => {
-  
-  if (openedCards < jsonData.length - 1) {
-  setOpenedCards(openedCards + 1);
-  }
-};
+  const [openedCards, setOpenedCards] = useState(0);
+  const [data, setData] = useState({});
+  const [unlockedModules, setUnlockedModules] = useState(0);
 
 
-useEffect(() => {
-  // let authToken = JSON.parse(localStorage.getItem("AUTH"));
-  // console.log(authToken.token, "token");
-  customAxios
-    .get("/pathways/c4ca/modules", {
-      headers: {
-        Authorization:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE4MyIsImVtYWlsIjoia29tYWxhQG5hdmd1cnVrdWwub3JnIiwiaWF0IjoxNjk4MDc5OTcwLCJleHAiOjE3Mjk2Mzc1NzB9.hR3m5DtqHTq3jsOMnaZ4laQSsZxyk_a8_y7jZC0YKz4",
+
+  useEffect(() => {
+    // let authToken = JSON.parse(localStorage.getItem("AUTH"));
+    // console.log(authToken.token, "token");
+    customAxios
+      .get("/pathways/c4ca/modules", {
+        headers: {
+          Authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtX2lkIjo3MywiZmxhZyI6ImM0Y2EiLCJpYXQiOjE2OTgzMzg3NjksImV4cCI6MTcyOTg5NjM2OX0.AMPzvRZVi9H0jjwMNY0YPpkCeeo0Ky8zB_YAfqKJn2w",
           // Authorization: `Bearer ${authToken.token}`,
-      },
-    })
-    .then((res) => {
-      console.log(res, "data");
-      setData(res.data.data);
-    
-    })
-    .catch((err) => {
-      console.log("error", err);
-      
-    });
-}, []);
+        },
+      })
+      .then((res) => {
+        console.log(res, "data");
+        setData(res.data.data);
+        setOpenedCards(
+          res.data.data.modules.filter((module) => {
+            return module.completed_portion === 100;
+          }).length
+        );
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }, []);
 
+  return (
+    <>
+      <Box sx={{ display: "flex", mt: 3, mb: "32px" }}>
+        <Typography variant="h6">
+          Coding for Climate Action Learning Path
+        </Typography>
+      </Box>
 
-console.log(data, "all data--");
+      {data &&
+        data.modules &&
+        data.modules.map((module, index) => (
+          <Card
+            key={index}
+            sx={{
+              border: 1,
+              borderColor: module.borderColor,
+              backgroundColor: module.color,
 
-return (
-<>
-  <Box sx={{ display: 'flex', mt: 3, mb: '32px' }}>
-    <Typography variant='h6'>Coding for Climate Action Learning Path</Typography>
-  </Box>
+              borderRadius: "8px",
+              mb: "32px",
+              width: "96%",
+              filter: openedCards >= index ? "none" : "grayscale(100%)",
+            }}
+          >
+            <CardContent>
+              <Grid container justifyContent="space-around">
+                <Grid item>
+                  <CardMedia
+                    component="img"
+                    alt="Image"
+                    height="100"
+                    image={module.logo}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={8} sx={{ mb: 2 }}>
+                  <Typography variant="subtitle1">{module.name}</Typography>
+                  <Typography variant="body1" sx={{ whiteSpace: "pre-wrap" }}>
+                    {module.description}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                    <CircularProgress
+                      variant="determinate"
+                      value={module.completed_portion}
+                      size={30}
+                      thickness={6}
+                    />
+                    <Typography variant="body1" sx={{ marginLeft: "5px" }}>
+                      {openedCards >= index && module.completed_portion !== ""
+                        ? `${module.completed_portion}%`
+                        : module.completed_portion !== "" && "Not Started"}
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Grid>
 
-  {data && data.modules && data.modules.map((module, index) => (
-  <Card
-    key={index}
-    sx={{
-    border: 1,
-    // borderColor: item.borderColor,
-    // backgroundColor: item.backgroundColor,
-    borderRadius: '8px',
-    mb: '32px',
-    width: '96%',
-    filter: openedCards >= index ? 'none' : 'grayscale(100%)',
-    }}
-    >
-    <CardContent>
-      <Grid container justifyContent='space-around'>
-        <Grid item>
-          <CardMedia
-            component='img'
-            alt='Image'
-            height='100'
-            image={module.logo}
-            // style={{ filter: openedCards >= index ? 'none' : 'grayscale(100%)' }}
-            />
-        </Grid>
-        <Grid item xs={12} sm={8} sx={{ mb: 2 }}>
-          <Typography variant='subtitle1'>{module.name}</Typography>
-          <Typography variant='body1' sx={{ whiteSpace: 'pre-wrap' }}>
-            {module.description}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <CircularProgress variant='determinate' value={module.completed_portion} size={30} thickness={6}  />
-            <Typography variant='body1' sx={{ marginLeft: '5px' }}>
-            {module.completed_portion}%
-              {/* {openedCards >= index &&item.progress !== "" ?`${item.progress }%` :item.progress !== "" && 'Not Started'} */}
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  pr: 2,
+                  pb: 2,
+                  mt: "16px",
+                }}
+              >
+                <Button variant="outlined" disabled={openedCards < index}>
+                  {openedCards >= index ? (
+                    module.button_type === "project_solution" ? (
+                      "Submit Project Topic"
+                    ) : module.button_type === "project_topic" ? (
+                      "Share Project Topic"
+                    ) : (
+                      "Learn on Meraki"
+                    )
+                  ) : (
+                    <>
+                      <LockIcon />
+                      &nbsp;
+                      {module.button_type === "project_solution"
+                        ? "Submit Project Topic"
+                        : module.button_type === "project_topic"
+                        ? "Share Project Topic"
+                        : "Learn on Meraki"}
+                    </>
+                  )}
+                </Button>
 
-      {/* <Box sx={{ display: 'flex', justifyContent: 'flex-end', pr: 2, pb: 2, mt: '16px' }}>
-        <Button
-        variant='outlined'
-        disabled={openedCards < index}
-        endIcon={openedCards >= index && item.buttonText === "Learn on Meraki" &&<LaunchIcon /> }
-        onClick={openedCards === index ? handleCardOpen : null}
-        >
-          {openedCards >= index ? (
-          item.buttonText
-          ) : (
-          <><LockIcon />&nbsp;{item.buttonText}</>
-          )}
-        </Button>
-      </Box> */}
-    </CardContent>
-  </Card>
-  ))}
+                {/* <Button
+                  variant="outlined"
+                  disabled={openedCards < index}
+                  endIcon={
+                    openedCards >= index &&
+                    item.buttonText === "Learn on Meraki" && <LaunchIcon />
+                  }
+                  onClick={openedCards === index ? handleCardOpen : null}
+                >
+                  {openedCards >= index ? (
+                    module.button_type
 
-  <Box sx={{ display: 'block', textAlign: 'center', mt: '32px', mb: '32px' }}>
-  <img src={'/assets/separator.svg'} alt='Image' style={{ width: '5' }} />
-  </Box>
-  <Box sx={{ display: 'block', textAlign: 'center', mt: 3, mb: 5 }}>
-  <img src={'/assets/Frame.svg'} alt='Image' style={{ width: '40%' }} />
-  </Box>
-  </>
-);
+                  ) : (
+                    <>
+                      <LockIcon />
+                      &nbsp;{item.buttonText}
+                    </>
+                  )}
+                </Button> */}
+              </Box>
+            </CardContent>
+          </Card>
+        ))}
+
+      <Box
+        sx={{ display: "block", textAlign: "center", mt: "32px", mb: "32px" }}
+      >
+        <img src={"/assets/separator.svg"} alt="Image" style={{ width: "5" }} />
+      </Box>
+      <Box sx={{ display: "block", textAlign: "center", mt: 3, mb: 5 }}>
+        <img src={"/assets/Frame.svg"} alt="Image" style={{ width: "40%" }} />
+      </Box>
+    </>
+  );
 };
 
 export default Module;
-
-
-
-
--- pages/index.js
+++ pages/index.js
@@ -15,16 +15,12 @@ export default function Home() {
   // const isMobile = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)")
   return (
     <>
-      <Container sx={{ marginTop: "10%" }} maxWidth="lg">
-        hey komal
-      </Container>
-
-      {/* <center style={{ padding: 50 }}>
+      <center style={{ padding: 50 }}>
       <Typography variant="h4">CODE 4 CLIMATE ACTION</Typography>
       <br />
       <Typography color="red" variant="subtitle1">Opps! Site under maintanance</Typography>
       <Typography color="gray" variant="caption">We apologize for any inconveniences caused we've almost done. </Typography>
-    </center> */}
+    </center>
     </>
   );
 }
