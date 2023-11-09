004e805b1066a39a83f3ac1c4f6877e568a21592
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-11-09 22:35:06 +0530

    addded meraki url

--- components/studentDashboard/Module.jsx
+++ components/studentDashboard/Module.jsx
@@ -118,7 +118,7 @@ const Module = () => {
                       "Share Project Topic"
                     ) : (
                       <>
-                      <a  href={`http://localhost:3001/?studentAuth=${token}`}  target="_blank">Learn on Meraki <LaunchIcon /></a>  
+                      <a  href={`https://www.merd-bhanwaridevi.merakilearn.org/?studentAuth=${token}`}  target="_blank">Learn on Meraki <LaunchIcon /></a>  
                       </>
                     )
                   ) : (
